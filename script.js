79f69bf96d80628148d7d4196af6c13d160ce8ee
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-06-27 13:27:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function size(){
+	document.getElementById("width").innerHTML = window.innerWidth;
+	document.getElementById("height").innerHTML = window.innerHeight;
+}
+
+window.addEventListener('load',size);
+window.addEventListener('resize',size);
